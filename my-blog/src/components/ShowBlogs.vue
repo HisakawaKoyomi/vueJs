430af6c70104cfc4b003c716c0ba809f3fc0d160
HisakawaKoyomi <14yli3@stu.edu.cn>
2019-04-29 12:33:35 +0800

    showBlogs finished #dorm -2019.4.29

--- my-blog/src/components/ShowBlogs.vue
+++ my-blog/src/components/ShowBlogs.vue
@@ -1,10 +1,11 @@
 <template>
-  <div id="show-blogs">
+  <div id="show-blogs" v-theme:column="'wide'">
     <h1>博客总览</h1>
-    <div class="single-blog" v-for="blog in blogs">
-      <h2>{{blog.title}}</h2>
+    <input type="text" placeholder="搜索" v-model="search">
+    <div class="single-blog" v-for="blog in filteredBlogs">
+      <h2 v-rainbow>{{blog.title | to-uppercase}}</h2>
       <article>
-        {{blog.body}}
+        {{blog.body | snippet}}
       </article>
     </div>
   </div>
@@ -13,9 +14,10 @@
 <script>
     export default {
       name: "show-blogs",
-      data () {
+      data() {
         return {
-          blogs: []
+          blogs: [],
+          search: ""
         }
       },
       created() {
@@ -23,18 +25,58 @@
           .then(function (data) {
             this.blogs = data.body.slice(0,10);
           })
+      },
+      computed: {
+        filteredBlogs() {
+          return this.blogs.filter((blog) => {
+            return blog.title.match(this.search);
+          })
+        }
+      },
+      filters: {
+        toUppercase(value) {
+          return value.toUpperCase()
+        },
+        snippet(value) {
+          return value.slice(0,100) + "..."
+        }
+      },
+      directives: {
+        rainbow: {
+          bind(el,binding,vnode) {
+            el.style.color = "#" + Math.random().toString(16).slice(2,8);
+          }
+        },
+        theme: {
+          bind(el,binding,vnode) {
+            if (binding.value === "wide") {
+              el.style.maxWidth = '1026 px';
+            }
+            if (binding.arg === "column") {
+              el.style.background = "#6677cc";
+              el.style.padding = "20 px";
+            }
+          }
+        }
       }
     }
 </script>
 
 <style scoped>
   #show-blogs {
-  max-width: 800px;
-  margin: 0 auto;
-}
+    max-width: 800px;
+    margin: 0 auto;
+    padding: 10px;
+  }
+  #show-blogs h1 {
+    padding: 0 20px;
+  }
+  #show-blogs input {
+    margin: 0 20px;
+  }
   .single-blog {
     padding: 20px;
-    margin: 20px 0;
+    margin: 20px;
     box-sizing: border-box;
     background: #eee;
   }
--- my-blog/src/main.js
+++ my-blog/src/main.js
@@ -4,13 +4,30 @@ import Vue from 'vue'
 import App from './App'
 import VueResource from 'vue-resource'
 
+Vue.config.productionTip = false
+
+// 全局使用ajax插件
 Vue.use(VueResource);
 
-Vue.config.productionTip = false
+/*
+// 自定义全局指令 -> 后改为了局部
+Vue.directive('rainbow',{
+  bind (el,binding,vnode) {
+    el.style.color = '#' + Math.random().toString(16).slice(2,8);
+    //el.style.color = 'blue';
+  }
+});
+
+// 自定义全局过滤器 -> 后改为了局部
+Vue.filter('to-uppercase',function (value) {
+  return value.toLowerCase()
+});
+*/
+
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   components: { App },
   template: '<App/>'
-})
+});
